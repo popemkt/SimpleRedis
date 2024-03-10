3ea06d487370b781850abcc9229cbc3e0c3ff213
popemkt <hoangng71299@gmail.com>
2024-03-10 23:46:10 +0700

    multiresponse

--- src/Server.cs
+++ src/Server.cs
@@ -5,9 +5,10 @@ using System.Text;
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
+var socket = server.AcceptSocket(); // wait for client
+var buffer = new byte[1024];
 while (true)
 {
-    var socket = server.AcceptSocket(); // wait for client
+    _ = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
     await socket.SendAsync((ArraySegment<byte>)Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
-    socket.Close();
 }
