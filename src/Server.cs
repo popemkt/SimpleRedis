cf5cf94d1a8de5a0d9595498da8fad7b4c0f643b
popemkt <hoangng71299@gmail.com>
2024-03-10 23:38:57 +0700

    multi

--- src/Server.cs
+++ src/Server.cs
@@ -5,5 +5,9 @@ using System.Text;
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-var socket = server.AcceptSocket(); // wait for client
-socket.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+while (true)
+{
+    var socket = server.AcceptSocket(); // wait for client
+    await socket.SendAsync((ArraySegment<byte>)Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+    socket.Close();
+}
