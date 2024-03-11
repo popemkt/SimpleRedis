5754668cb4b720cdd8be7a7a57619c6bc5cdcee1
popemkt <hoangng71299@gmail.com>
2024-03-11 23:48:13 +0700

    event loop style

--- src/Server.cs
+++ src/Server.cs
@@ -5,17 +5,19 @@ using System.Text;
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
+
 while (true)
 {
-    var socket = server.AcceptSocket(); // wait for client
-    await Task.Run(() => HandleClient(socket));
-    async Task HandleClient(Socket socket)
+    var socket = await server.AcceptSocketAsync(); // wait for client
+    _ = HandleClient(socket);
+}
+
+async Task HandleClient(Socket socket)
+{
+    var buffer = new byte[1024];
+    while (true)
     {
-        var buffer = new byte[1024];
-        while (true)
-        {
-            _ = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
-            await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
-        }
+        _ = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
+        await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
     }
 }
