a52d54e0c91e5fbe7924c37d1bf61e57f2d4d245
popemkt <hoangng71299@gmail.com>
2024-03-11 23:35:27 +0700

    await

--- src/Server.cs
+++ src/Server.cs
@@ -5,10 +5,17 @@ using System.Text;
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-var socket = server.AcceptSocket(); // wait for client
-var buffer = new byte[1024];
 while (true)
 {
-    _ = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
-    await socket.SendAsync((ArraySegment<byte>)Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+    var socket = server.AcceptSocket(); // wait for client
+    Task.Run(() => HandleClient(socket));
+    async Task HandleClient(Socket socket)
+    {
+        var buffer = new byte[1024];
+        while (true)
+        {
+            _ = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
+            await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+        }
+    }
 }
